6fdc5d66664a1fcf14898aa88ba905628d9db283
xzjs <2310976219@qq.com>
2017-11-13 17:22:46 +0800

    添加竖屏界面

--- app/src/main/java/love/xzjs/t_android/MainActivity.java
+++ app/src/main/java/love/xzjs/t_android/MainActivity.java
@@ -1,31 +1,35 @@
 package love.xzjs.t_android;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
+        int mCurrentOrientation=getResources().getConfiguration().orientation;
+        if(mCurrentOrientation== Configuration.ORIENTATION_PORTRAIT){
+            Log.i("info","port");
+            setContentView(R.layout.config);
+        }else{
+            Log.i("info","land");
+            setContentView(R.layout.activity_main);
+        }
     }
 
     @Override
