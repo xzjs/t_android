234130aae930fee9224596fe0330484393ea8577
xzjs <2310976219@qq.com>
2017-11-21 11:41:29 +0800

    完成用switch控制的UI的显示与隐藏

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # t_android
 小t的安卓端
+# 教程地址
+[做一款自己的安卓天气闹钟（1）——首页界面布局](http://www.jianshu.com/p/a9598a5e300d)
+
+[做一款自己的安卓天气闹钟（2）——顶部导航栏+ViewPager滑动切换页面](http://www.jianshu.com/p/5dd8397f8e70)
+
+[做一款自己的安卓天气闹钟（3）——用Switch控制元素的显示隐藏](http://www.jianshu.com/p/2079cc363c88)
--- app/src/main/java/love/xzjs/t_android/Show.java
+++ app/src/main/java/love/xzjs/t_android/Show.java
@@ -9,15 +9,15 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.CompoundButton;
+import android.widget.LinearLayout;
 import android.widget.Switch;
 import android.widget.TextView;
 
-/**
- * Created by Administrator on 2017/11/16.
- */
+
 
 public class Show extends Fragment implements CompoundButton.OnCheckedChangeListener {
     private TextView textViewTime, textViewDate, textViewWeek;
+    private LinearLayout linearLayout1,linearLayout2,linearLayout3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -26,44 +26,58 @@ public class Show extends Fragment implements CompoundButton.OnCheckedChangeList
         textViewTime = (TextView) view.findViewById(R.id.time_label);
         textViewDate = (TextView)view.findViewById(R.id.date);
         textViewWeek = (TextView)view.findViewById(R.id.week);
+        linearLayout1=(LinearLayout)view.findViewById(R.id.weather_today);
+        linearLayout2=(LinearLayout)view.findViewById(R.id.weather_tomorrow);
+        linearLayout3=(LinearLayout)view.findViewById(R.id.weather_after_tomorrow);
 
+        //根据屏幕宽度确定字体的大小
         Resources resources = this.getResources();
         DisplayMetrics dm = resources.getDisplayMetrics();
         int width = dm.widthPixels;
         textViewTime.setTextSize(TypedValue.COMPLEX_UNIT_SP, 150 * width / 1794);
 
+        //绑定switch事件
         Switch switchTime = (Switch) view.findViewById(R.id.switch_time);
         Switch switchDate = (Switch) view.findViewById(R.id.switch_date);
         Switch switchWeek = (Switch) view.findViewById(R.id.switch_week);
+        Switch switchWeather1=(Switch)view.findViewById(R.id.switch_weather1);
+        Switch switchWeather2=(Switch)view.findViewById(R.id.switch_weather2);
+        Switch switchWeather3=(Switch)view.findViewById(R.id.switch_weather3);
         switchTime.setOnCheckedChangeListener(this);
         switchDate.setOnCheckedChangeListener(this);
         switchWeek.setOnCheckedChangeListener(this);
+        switchWeather1.setOnCheckedChangeListener(this);
+        switchWeather2.setOnCheckedChangeListener(this);
+        switchWeather3.setOnCheckedChangeListener(this);
+
         return view;
     }
 
+    /**
+     * 重写switch状态变更事件
+     * @param compoundButton
+     * @param b
+     */
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
         switch (compoundButton.getId()) {
             case R.id.switch_time:
-                if (compoundButton.isChecked()) {
-                    textViewTime.setVisibility(View.VISIBLE);
-                } else {
-                    textViewTime.setVisibility(View.INVISIBLE);
-                }
+                textViewTime.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
                 break;
             case R.id.switch_date:
-                if (compoundButton.isChecked()) {
-                    textViewDate.setVisibility(View.VISIBLE);
-                } else {
-                    textViewDate.setVisibility(View.INVISIBLE);
-                }
+                textViewDate.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
                 break;
             case R.id.switch_week:
-                if (compoundButton.isChecked()) {
-                    textViewWeek.setVisibility(View.VISIBLE);
-                } else {
-                    textViewWeek.setVisibility(View.INVISIBLE);
-                }
+                textViewWeek.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                break;
+            case R.id.switch_weather1:
+                linearLayout1.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                break;
+            case R.id.switch_weather2:
+                linearLayout2.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                break;
+            case R.id.switch_weather3:
+                linearLayout3.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
                 break;
         }
     }
