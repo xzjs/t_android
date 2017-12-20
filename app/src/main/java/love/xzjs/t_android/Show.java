85f134505f04a204c0075bef1758cd1b34533694
xzjs <2310976219@qq.com>
2017-12-20 14:43:55 +0800

    使用SQLLite存储配置数据

--- app/src/main/java/love/xzjs/t_android/Show.java
+++ app/src/main/java/love/xzjs/t_android/Show.java
@@ -1,6 +1,7 @@
 package love.xzjs.t_android;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -18,8 +19,10 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.CompoundButton;
 import android.widget.LinearLayout;
+import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
 
@@ -28,16 +31,26 @@ import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
 import com.baidu.location.LocationClientOption;
 
-public class Show extends Fragment implements CompoundButton.OnCheckedChangeListener, BDLocationListener {
-    private TextView textViewTime, textViewDate, textViewWeek,_locationTextView;
-    private Switch switchTime,switchDate,switchWeek,switchWeather1,switchWeather2,switchWeather3;
-    private LinearLayout linearLayout1,linearLayout2,linearLayout3;
+import java.util.HashMap;
+
+public class Show extends Fragment implements CompoundButton.OnCheckedChangeListener, BDLocationListener, AdapterView.OnItemSelectedListener {
+    private TextView textViewTime, textViewDate, textViewWeek, _locationTextView;
+    private Switch switchTime, switchDate, switchWeek, switchWeather1, switchWeather2, switchWeather3;
+    private LinearLayout linearLayout1, linearLayout2, linearLayout3;
+    private Spinner spinner;
 
     private LocationClient _locationClient;
-    private static final int BAIDU_READ_PHONE_STATE =100;
+    private static final int BAIDU_READ_PHONE_STATE = 100;
 
     private MyDBOpenHelper myDBOpenHelper;
-    private Config config;
+    private int id;
+    private ContentValues values;
+    private HashMap<String, TextView> textViewHashMap;
+    private HashMap<String, Switch> switchHashMap;
+    private HashMap<String, LinearLayout> linearLayoutHashMap;
+
+    //判断是否为刚进去时触发onItemSelected的标志
+    private boolean spinnerSelected = false;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -45,12 +58,14 @@ public class Show extends Fragment implements CompoundButton.OnCheckedChangeList
         View view = inflater.inflate(R.layout.config_show, container, false);
 
         textViewTime = (TextView) view.findViewById(R.id.time_label);
-        textViewDate = (TextView)view.findViewById(R.id.date);
-        textViewWeek = (TextView)view.findViewById(R.id.week);
-        linearLayout1=(LinearLayout)view.findViewById(R.id.weather_today);
-        linearLayout2=(LinearLayout)view.findViewById(R.id.weather_tomorrow);
-        linearLayout3=(LinearLayout)view.findViewById(R.id.weather_after_tomorrow);
-        _locationTextView=view.findViewById(R.id.locationTextView);
+        textViewDate = (TextView) view.findViewById(R.id.date);
+        textViewWeek = (TextView) view.findViewById(R.id.week);
+        linearLayout1 = (LinearLayout) view.findViewById(R.id.weather_today);
+        linearLayout2 = (LinearLayout) view.findViewById(R.id.weather_tomorrow);
+        linearLayout3 = (LinearLayout) view.findViewById(R.id.weather_after_tomorrow);
+        _locationTextView = view.findViewById(R.id.locationTextView);
+        spinner = view.findViewById(R.id.spinner1);
+        values = new ContentValues();
 
         //根据屏幕宽度确定字体的大小
         Resources resources = this.getResources();
@@ -62,9 +77,9 @@ public class Show extends Fragment implements CompoundButton.OnCheckedChangeList
         switchTime = (Switch) view.findViewById(R.id.switch_time);
         switchDate = (Switch) view.findViewById(R.id.switch_date);
         switchWeek = (Switch) view.findViewById(R.id.switch_week);
-        switchWeather1=(Switch)view.findViewById(R.id.switch_weather1);
-        switchWeather2=(Switch)view.findViewById(R.id.switch_weather2);
-        switchWeather3=(Switch)view.findViewById(R.id.switch_weather3);
+        switchWeather1 = (Switch) view.findViewById(R.id.switch_weather1);
+        switchWeather2 = (Switch) view.findViewById(R.id.switch_weather2);
+        switchWeather3 = (Switch) view.findViewById(R.id.switch_weather3);
         switchTime.setOnCheckedChangeListener(this);
         switchDate.setOnCheckedChangeListener(this);
         switchWeek.setOnCheckedChangeListener(this);
@@ -72,9 +87,9 @@ public class Show extends Fragment implements CompoundButton.OnCheckedChangeList
         switchWeather2.setOnCheckedChangeListener(this);
         switchWeather3.setOnCheckedChangeListener(this);
 
-        _locationClient=new LocationClient(getContext());
+        _locationClient = new LocationClient(getContext());
         _locationClient.registerLocationListener(this);
-        LocationClientOption option=new LocationClientOption();
+        LocationClientOption option = new LocationClientOption();
         option.setIsNeedAddress(true);
         option.setLocationMode(LocationClientOption.LocationMode.Hight_Accuracy);//设置高精度定位定位模式
         option.setOpenGps(true); // 打开gps
@@ -82,32 +97,76 @@ public class Show extends Fragment implements CompoundButton.OnCheckedChangeList
         option.setScanSpan(1000);
         _locationClient.setLocOption(option);
 
-        if(ActivityCompat.checkSelfPermission(getActivity(),Manifest.permission.READ_PHONE_STATE)!= PackageManager.PERMISSION_GRANTED) {
+        if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
             // 申请一个（或多个）权限，并提供用于回调返回的获取码（用户定义）
-            requestPermissions( new String[]{ Manifest.permission.READ_PHONE_STATE },BAIDU_READ_PHONE_STATE );
-        }else{
+            requestPermissions(new String[]{Manifest.permission.READ_PHONE_STATE}, BAIDU_READ_PHONE_STATE);
+        } else {
             _locationClient.start();
         }
 
-        myDBOpenHelper=new MyDBOpenHelper(getActivity(),"clock.db",null,1);
-        SQLiteDatabase db=myDBOpenHelper.getWritableDatabase();
-        Cursor cursor=db.query("config",null,null,null,null,null,null);
-        if(cursor.moveToFirst()){
-            int id=cursor.getInt(cursor.getColumnIndex("id"));
-            int time=cursor.getInt(cursor.getColumnIndex("time"));
-            int date=cursor.getInt(cursor.getColumnIndex("date"));
-            int week=cursor.getInt(cursor.getColumnIndex("week"));
-            String location=cursor.getString(cursor.getColumnIndex("location"));
-            int firstDay=cursor.getInt(cursor.getColumnIndex("firstDay"));
-            int secondDay=cursor.getInt(cursor.getColumnIndex("secondDay"));
-            int thirdDay=cursor.getInt(cursor.getColumnIndex("thirdDay"));
-            int num=cursor.getInt(cursor.getColumnIndex("num"));
-            config=new Config(id,time,date,week,firstDay,secondDay,thirdDay,num,location);
-        }else{
+        textViewHashMap=new HashMap<>();
+        textViewHashMap.put("time", textViewTime);
+        textViewHashMap.put("date", textViewDate);
+        textViewHashMap.put("week", textViewWeek);
+        linearLayoutHashMap=new HashMap<>();
+        linearLayoutHashMap.put("firstDay", linearLayout1);
+        linearLayoutHashMap.put("secondDay", linearLayout2);
+        linearLayoutHashMap.put("thirdDay", linearLayout3);
+        switchHashMap=new HashMap<>();
+        switchHashMap.put("time", switchTime);
+        switchHashMap.put("date", switchDate);
+        switchHashMap.put("week", switchWeek);
+        switchHashMap.put("firstDay", switchWeather1);
+        switchHashMap.put("secondDay", switchWeather2);
+        switchHashMap.put("thirdDay", switchWeather3);
 
+        myDBOpenHelper = new MyDBOpenHelper(getActivity(), "clock.db", null, 1);
+        SQLiteDatabase db = myDBOpenHelper.getWritableDatabase();
+        Cursor cursor = db.query("config", null, null, null, null, null, null);
+        if (cursor.moveToFirst()) {
+            id = cursor.getInt(cursor.getColumnIndex("id"));
+            values.put("time", cursor.getInt(cursor.getColumnIndex("time")));
+            values.put("date", cursor.getInt(cursor.getColumnIndex("date")));
+            values.put("week", cursor.getInt(cursor.getColumnIndex("week")));
+            values.put("location", cursor.getInt(cursor.getColumnIndex("location")));
+            values.put("firstDay", cursor.getInt(cursor.getColumnIndex("firstDay")));
+            values.put("secondDay", cursor.getInt(cursor.getColumnIndex("secondDay")));
+            values.put("thirdDay", cursor.getInt(cursor.getColumnIndex("thirdDay")));
+            values.put("num", cursor.getInt(cursor.getColumnIndex("num")));
+            setViewData();
         }
 
         return view;
+    }
+
+    /**
+     * 绑定数据
+     */
+    private void setViewData() {
+        for (String key : values.keySet()) {
+            boolean status = (int) values.get(key) == 1;
+            switch (key) {
+                case "time":
+                case "date":
+                case "week":
+                    textViewHashMap.get(key).setVisibility(status ? View.VISIBLE : View.GONE);
+                    switchHashMap.get(key).setChecked(status);
+                    break;
+                case "firstDay":
+                case "secondDay":
+                case "thirdDay":
+                    linearLayoutHashMap.get(key).setVisibility(status ? View.VISIBLE : View.GONE);
+                    switchHashMap.get(key).setChecked(status);
+                    break;
+                case "num":
+                    spinnerSelected = false;
+                    int num = (int) values.get(key) - 1;
+                    spinner.setSelection(num);
+                    break;
+                default:
+                    break;
+            }
+        }
     }
 
     /**
@@ -118,36 +177,44 @@ public class Show extends Fragment implements CompoundButton.OnCheckedChangeList
     @Override
     public void onStop() {
         super.onStop();
-        if(config==null){
-            ContentValues values = new ContentValues();
-        }
+
+        SQLiteDatabase db = myDBOpenHelper.getWritableDatabase();
+        db.update("config", values, "id=?", new String[]{String.valueOf(id)});
     }
 
     /**
      * 重写switch状态变更事件
+     *
      * @param compoundButton
      * @param b
      */
     @Override
     public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        boolean isChecked = compoundButton.isChecked();
         switch (compoundButton.getId()) {
             case R.id.switch_time:
-                textViewTime.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                textViewTime.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                values.put("time", isChecked ? 1 : 0);
                 break;
             case R.id.switch_date:
-                textViewDate.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                textViewDate.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                values.put("date", isChecked ? 1 : 0);
                 break;
             case R.id.switch_week:
-                textViewWeek.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                textViewWeek.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                values.put("week", isChecked ? 1 : 0);
                 break;
             case R.id.switch_weather1:
-                linearLayout1.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                linearLayout1.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                values.put("firstDay", isChecked ? 1 : 0);
                 break;
             case R.id.switch_weather2:
-                linearLayout2.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                linearLayout2.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                values.put("secondDay", isChecked ? 1 : 0);
                 break;
             case R.id.switch_weather3:
-                linearLayout3.setVisibility(compoundButton.isChecked()?View.VISIBLE:View.GONE);
+                linearLayout3.setVisibility(isChecked ? View.VISIBLE : View.GONE);
+                values.put("thirdDay", isChecked ? 1 : 0);
                 break;
         }
     }
@@ -160,12 +227,13 @@ public class Show extends Fragment implements CompoundButton.OnCheckedChangeList
         String city = bdLocation.getCity();    //获取城市
         String district = bdLocation.getDistrict();    //获取区县
         String street = bdLocation.getStreet();    //获取街道信息
-        _locationTextView.setText(city+district);
+        _locationTextView.setText(city + district);
+        values.put("location", city + district);
         _locationClient.stop();
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode,String[] permissions, int[] grantResults) {
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         switch (requestCode) {
             //requestCode即所声明的权限获取码，在checkSelfPermission时传入
@@ -180,5 +248,19 @@ public class Show extends Fragment implements CompoundButton.OnCheckedChangeList
             default:
                 break;
         }
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        if (spinnerSelected) {
+            values.put("num", i + 1);
+        } else {
+            spinnerSelected = true;
+        }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
     }
 }
